8eef06f79f12550200b2e0a30e225aca52b2cb30
electrone901 <carbajalluis@rocketmail.com>
2021-08-04 18:24:06 -0400

    Added: react router using react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // import logo from './logo.svg';
 import './App.css'
+import Navbar from './components/layout/navbar/Navbar'
+import Footer from './components/layout/footer/Footer'
+import Home from './components/home/Home'
+import CreatePet from './components/create-post/CreatePet'
 
 import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>PetGram</h1>
-    </div>
+    <Router>
+      <div className="cl">
+        <Navbar />
+        <Route exact path="/home" component={Home} />
+        <Switch>
+          <Route exact path="/create-pet" component={CreatePet} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/create-post/CreatePet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreatePet() {
+  return (
+    <div>
+      <h1>CreatePet</h1>
+    </div>
+  )
+}
+
+export default CreatePet
