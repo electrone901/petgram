f3a1713953f2aaffe262fcb9c5045ce761ffeccd
electrone901 <carbajalluis@rocketmail.com>
2021-08-04 18:56:30 -0400

    Added: material-ui and material-ui icons

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,12 +1,16 @@
+import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar'
+
+
 import React from 'react'
 
 function Navbar() {
   return (
     <div>
+      <Button className="butt">Technology</Button>
       <h1>Navbar</h1>
     </div>
   )
 }
 
 export default Navbar
-
